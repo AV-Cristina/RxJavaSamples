8ecbf9c3aeb5f3ffda10c7787a669e93d923a3e7
cristina <afanasyeva.kv@open.ru>
2023-11-21 18:58:48 +0300

    CustomExecutorSample

---
+++ app/src/test/java/com/rxjavasamples/kotlin/operators/CustomExecutorSample.kt
@@ -0,0 +1,21 @@
+package com.rxjavasamples.kotlin.operators
+
+import android.annotation.SuppressLint
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.Test
+import java.util.concurrent.Executors
+
+class CustomExecutorSample {
+    @SuppressLint("CheckResult")
+    @Test
+    fun customExecutorSample() {
+        val numberOfThreads = 20
+        val executor = Executors.newFixedThreadPool(numberOfThreads)
+        val scheduler = Schedulers.from(executor)
+        Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+            .subscribeOn(scheduler)
+            .doFinally(executor::shutdown)
+            .subscribe { s -> println(s) }
+    }
+}
